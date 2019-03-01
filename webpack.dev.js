8e7064061b133da5119e72adfc6cd2e04e06b328
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-03-01 19:17:45 +0900

    Don't use tab for indentation! Use spaces!

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
-const webpack = require('webpack');
-const path = require('path');
+const webpack = require("webpack");
+const path = require("path");
 
 /*
  * SplitChunksPlugin is enabled by default and replaced
@@ -22,89 +22,85 @@ const path = require('path');
  *
  */
 
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, "src")],
+        loader: "babel-loader",
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+        options: {
+          plugins: ["syntax-dynamic-import"],
 
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
 
-				test: /\.js$/
-			},
-			{
-				use: ['vue-loader'],
-				test: /\.vue$/
+        test: /\.js$/
+      },
+      {
+        use: ["vue-loader"],
+        test: /\.vue$/
       },
       {
         test: /\.styl$/,
-        use: [
-          'style-loader', 'css-loader', 'stylus-loader'
-        ]
+        use: ["style-loader", "css-loader", "stylus-loader"]
       },
-			{
-				test: /\.(scss|css)$/,
+      {
+        test: /\.(scss|css)$/,
 
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader'
-					},
-					{
-						loader: 'sass-loader'
-					}
-				]
-			}
-		]
-	},
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      }
+    ]
+  },
 
-	entry: {
-    app: './src/app/index.js'
-	},
+  entry: {
+    app: "./src/app/index.js"
+  },
 
-	output: {
-		filename: '[name].js'
-	},
+  output: {
+    filename: "[name].js"
+  },
 
-	mode: 'development',
-	resolve: {
-		alias: {
-			'vue$': 'vue/dist/vue.esm.js'
-		}
-	},
-	plugins: [
-		new VueLoaderPlugin()
-	 ],
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+  mode: "development",
+  resolve: {
+    alias: {
+      vue$: "vue/dist/vue.esm.js"
+    }
+  },
+  plugins: [new VueLoaderPlugin()],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+      chunks: "async",
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
 };
