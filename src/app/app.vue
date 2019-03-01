0031aeb1e8a18a30a49874bd1bffbda24552af64
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-03-01 18:18:03 +0900

    Calculate BMR in The Mifflin St Jeor Equation

--- src/app/app.vue
+++ src/app/app.vue
@@ -1,17 +1,34 @@
 <template>
-    <div>
-        <h1>Calories Calculator</h1>
+  <div>
+    <h1>Calories Calculator</h1>
 
-        {{ message }}
+    <form @submit.prevent="calculate">
+      <input type="text" v-model="weight">
+      <input type="text" v-model="height">
+      <input type="text" v-model="age">
+
+      <button type="submit">Calculate</button>
+    </form>
+
+    <div>
+      {{ calculate() }} kcal
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            message: 'Hello, World!'
-        }
+  data() {
+    return {
+      weight: 63.0,
+      height: 168.0,
+      age: 37
+    };
+  },
+  methods: {
+    calculate() {
+      return (10.0 * this.weight + 6.25 * this.height - 5.0 * this.age)
     }
-}
+  }
+};
 </script>
