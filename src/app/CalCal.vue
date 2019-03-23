60ef13136ce1ce3c827e758f5463d5a58b34462b
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-03-23 23:43:28 +0900

    Add footer

--- src/app/CalCal.vue
+++ src/app/CalCal.vue
@@ -25,7 +25,9 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn @click="dialog = false">I got it!</v-btn>
+          <v-btn @click="dialog = false">
+            <v-icon>mdi-thumb-up-outline</v-icon> I got it!
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -69,6 +71,17 @@
         </v-layout>
       </v-container>
     </v-content>
+    <v-footer app dark>
+      <v-spacer></v-spacer>
+
+      <v-btn icon flat href="https://twitter.com/sato_ryu">
+        <v-icon>mdi-twitter-circle</v-icon>
+      </v-btn>
+      <v-btn icon href="https://github.com/satoryu/CalCal">
+        <v-icon>mdi-github-circle</v-icon>
+      </v-btn>
+      <v-icon>mdi-copyright</v-icon> @sato_ryu
+    </v-footer>
   </v-app>
 </template>
 
